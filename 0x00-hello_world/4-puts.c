b5571009c523ab6e8e997c05c679ff60bb229311
yabasahilda2004 <yabasahilda2004@gmail.com>
2023-06-15 11:41:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 
 /**
- * main - prints  "Programming is like building a multilingual puzzle"
- * return 0.
+ * main - prints  "Programming is like building a multilingual puzzle,
+ *  followed by a newline"
+ * Return: Always 0.
  */
 
 int main(void)
